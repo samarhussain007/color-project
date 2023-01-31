6b8f96f794a3360089b067434e4bfdb9d63bd7e6
samarhussain007 <hussainsamar36@gmail.com>
2023-02-01 00:02:59 +0530

    ADD react router

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Pallete from "./pallete";
 import seedColors from "./seedColors";
 import { generatedPallete } from "./ColorHelper";
 
 function App() {
+  const Hello = () => <h1>hey there</h1>;
   return (
-    <div className="App">
-      <Pallete palette={generatedPallete(seedColors[4])} />
-    </div>
+    <Routes>
+      <Route path="/" />
+      <Route path="/palette/:id" element={<Hello />} />
+    </Routes>
   );
 }
 
