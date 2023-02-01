97a2ec2e79ceccc8773f0f4277c2ff9ef621e35c
samarhussain007 <hussainsamar36@gmail.com>
2023-02-02 01:11:07 +0530

    Fix Pallete routes

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -1,16 +1,19 @@
 import logo from "./logo.svg";
-import { Routes, Route } from "react-router-dom";
+
+import { Routes, Route, useParams } from "react-router-dom";
 import "./App.css";
 import Pallete from "./pallete";
 import seedColors from "./seedColors";
 import { generatedPallete } from "./ColorHelper";
 
 function App() {
-  const Hello = () => <h1>hey there</h1>;
   return (
     <Routes>
       <Route path="/" />
-      <Route path="/palette/:id" element={<Hello />} />
+      <Route
+        path="/palette/:id"
+        element={<Pallete palette={generatedPallete(...seedColors)} />}
+      />
     </Routes>
   );
 }
--- color-app/src/pallete.js
+++ color-app/src/pallete.js
@@ -1,42 +1,76 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ColorBox from "./ColorBox";
 import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
 import "./Pallete.css";
 import Navbar from "./Navbar";
+import { useParams } from "react-router-dom";
+import seedColors from "./seedColors";
+import { generatedPallete } from "./ColorHelper";
+// class Pallete extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { level: 500, format: "hex" };
+//     this.changeLevel = this.changeLevel.bind(this);
+//     this.changeFormat = this.changeFormat.bind(this);
+//   }
+//   changeLevel(newLevel) {
+//     this.setState({ level: newLevel });
+//   }
 
-class Pallete extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { level: 500, format: "hex" };
-    this.changeLevel = this.changeLevel.bind(this);
-    this.changeFormat = this.changeFormat.bind(this);
+//   changeFormat(evt) {
+//     this.setState({ format: evt });
+//   }
+
+//   render() {
+//     const { colors } = this.props.palette;
+//     const { level, format } = this.state;
+//     const colorBoxes = colors[level].map((el) => {
+//       return <ColorBox background={el[format]} name={el.name} />;
+//     });
+//     return (
+//       <div className="Pallete">
+//         <Navbar
+//           level={level}
+//           changeLevel={this.changeLevel}
+//           handleChange={this.changeFormat}
+//         />
+//         <div className="Pallete-colors">{colorBoxes}</div>
+//         {/* footer eventually */}
+//       </div>
+//     );
+//   }
+// }
+
+function Pallete(props) {
+  const { id } = useParams();
+  const findPallete = (id) => seedColors.find((palette) => palette.id === id);
+  const palette = generatedPallete(findPallete(id));
+
+  const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState("hex");
+  const { colors } = palette;
+
+  const colorBoxes = colors[level].map((el) => {
+    return <ColorBox background={el[format]} name={el.name} />;
+  });
+  function changeLevel(newLevel) {
+    setLevel(newLevel);
   }
-  changeLevel(newLevel) {
-    this.setState({ level: newLevel });
+  function changeFormat(evt) {
+    setFormat(evt);
   }
 
-  changeFormat(evt) {
-    this.setState({ format: evt });
-  }
-
-  render() {
-    const { colors } = this.props.palette;
-    const { level, format } = this.state;
-    const colorBoxes = colors[level].map((el) => {
-      return <ColorBox background={el[format]} name={el.name} />;
-    });
-    return (
-      <div className="Pallete">
-        <Navbar
-          level={level}
-          changeLevel={this.changeLevel}
-          handleChange={this.changeFormat}
-        />
-        <div className="Pallete-colors">{colorBoxes}</div>
-        {/* footer eventually */}
-      </div>
-    );
-  }
+  return (
+    <div className="Pallete">
+      <Navbar
+        level={level}
+        changeLevel={changeLevel}
+        handleChange={changeFormat}
+      />
+      <div className="Pallete-colors">{colorBoxes}</div>
+      {/* footer eventually */}
+    </div>
+  );
 }
 export default Pallete;
